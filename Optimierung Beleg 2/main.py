6b66e5e1b3f46198bfe28d6d8b73ba16f0b04842
Kryol1th <nicolaswinkler.nw@gmail.com>
2023-04-11 15:43:39 +0200

    nice

--- Optimierung Beleg 2/main.py
+++ Optimierung Beleg 2/main.py
@@ -1,62 +1,85 @@
-
 import numpy as np                            # Paket für numerische Operationen
-# import matplotlib.pyplot as plt             # Paket fürs grafische Darstellen
-# from matplotlib import rc
-# from gooey import Gooey, GooeyParser        # grafische Benutzeroberfläche (GUI)
+from numpy import ndarray
 
 # Startvektor:
-v_0 = np.array([0.5, 0.5])
-v_1 = np.array([100, 100])
+v_1: ndarray = np.array([0.5, 0.5])
+v_0: ndarray = np.array([100.0, 100.0])
 
 # Nebenbedingungen
-NB = np.array([0.25, 1])
+NB = np.array([0.25, 1.0])
 
 # Schrittweite
 SW = np.array([0.01, 0.005, 0.0025, 0.001])
 
-
+#Vektor zur Visualisierung
+X_mem = np.array([0.5, 0.5])
+print(X_mem)
 # Abbruchbedingung
-
 def found_optimum(v_0, v_1):
-    if abs(v_0.zielfunktion() - v_1.zielfunktion()) < 0.01:
+    if abs(zielfunktion(v_0) - zielfunktion(v_1)) < 0.01:
         return True
     else:
         return False
 
-def gradient (v):
-    gradient = 1
-    return gradient
+#Funktion zur Berechnung des Gradienten
+def getGradient (v_1):
+    grd: ndarray = np.array([0, 0])
+    grd[0] = 100 * v_1[1] + 1 / v_1[1] - 1 / (v_1[0] ** 2 * v_1[1])
+    grd[1] = 100 * v_1[0] - (v_1[0]/(v_1[1])**2) - 1 / (v_1[0] * v_1[1] ** 2)
+    return grd
 
-i = 0
-gradient = v_0.gradient()
+#Funktion zur Berechnung des Aktuellen Wertes der Zielfunktion
+def zielfunktion(v_1):
+    z = 100 * v_1[0] * v_1[1] + (v_1[0]/v_1[1]) + 1 / (v_1[0] * v_1[1])
+    return z
 
+#Iterative Schleife, bis Minimal Wert der Zielfunktion gefunden wurde
+i = 1
+gradient = getGradient(v_1)
 while not found_optimum(v_0, v_1):
-    v_temp = v_1+1
-    print(v_temp.x1, v_temp.x2)
-    v_1.x1 = v_0.x1 - SW[1] * gradient[0]
-    v_1.x2 = v_0.x2 - SW[1] * gradient[1]
-    print(v_temp.x1, v_temp.x2)
+    v_0 = np.copy(v_1)
+#Berechnung des neuen Punktes
+    v_1[0] = v_0[0] - SW[i] * gradient[0]
+    v_1[1] = v_0[1] - SW[i] * gradient[1]
 
-    print(v_1.x1, v_1.x2)
-    print(v_0.x1, v_0.x2)
-
-    if v_0.zielfunktion() < v_1.zielfunktion():
-        gradient = v_0.gradient()
+    X_mem = np.vstack((X_mem, v_1))
+    """
+    print("-----------------")
+    print(gradient)
+    print(v_1[0], v_1[1])
+    print(zielfunktion(v_1))
+    print(zielfunktion(v_0))
+    print(i)
+    """
+#Anpasung des Gradientens und der Schrittweite
+    if zielfunktion(v_1) < zielfunktion(v_0):
+        gradient = getGradient(v_1)
         if i < 3:
             i = i + 1
-    if v_0.zielfunktion() > v_1.zielfunktion():
-        if i > 0:
+    if zielfunktion(v_1) >= zielfunktion(v_0):
+        if i >= 0:
             i = i - 1
-    if v_1.x1 < NB[0] or v_1.x1 > NB[1] or v_1.x2 < NB[0] or v_1.x2 > NB[1]:
-        if i > 0:
+
+#Einhalten der Nebenbedingungen
+    if v_1[0] <= NB[0] or v_1[0] >= NB[1] or v_1[1] <= NB[0] or v_1[1] >= NB[1]:
+        if i >= 0:
             i = i - 1
-    print(v_temp.x1, v_temp.x2)
-    v_0 = v_temp
-    print(v_0.x1, v_0.x2)
-    print(v_temp.x1, v_temp.x2)
 
-    print(v_0.zielfunktion())
-    print(v_1.zielfunktion())
-    print(v_0.zielfunktion() - v_1.zielfunktion())
-    print('---------------------------')
 
+#Ergebniss Ausgabe
+print("---------------------------------------")
+print("LÖSUNG:")
+print("Iterationsablauf:", X_mem)
+print("X1 =", v_1[0])  # WARUM IST DER WERT GERUNDET?????
+print("X2 =", v_1[1])
+print("Minimaler Wert der Zielfunktion:", zielfunktion(v_1))
+print("---------------------------------------")
+
+# Erstellung Plot
+x_coordinates = [M_min, M_max]
+y_coordinates = [alpha, alpha]
+plt.plot(x_coordinates, y_coordinates, color="navy")
+# Ausgabe Plot
+#plt.show()
+plt.title("Genetic")
+plt.savefig("plot_genetic.png")
